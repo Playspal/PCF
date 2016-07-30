be1f69b877ae8178fbd48cf8521f9e2d452bd1f3
Playspal <playspal@gmail.com>
2016-07-30 17:30:05 +0300

    Ui settings added

---
+++ ui/UiSettings.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace com.playspal.core.ui
+{
+    public class UiSettings
+    {
+        public static string DefaultClickSound;
+        public static string DefaultSwipeSound;
+        public static string DefaultPopupSound;
+
+        public static float DefinedScreenWidth = 1280f;             
+    }
+}
