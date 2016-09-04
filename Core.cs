dd5eafdb9cae17476e5a7214753e341689567cbe
Playspal <playspal@gmail.com>
2016-09-04 21:31:02 +0300

    Game storage updated

--- Core.cs
+++ Core.cs
@@ -2,12 +2,23 @@
 using System.Collections;
 
 using com.playspal.core.system.services.deffered;
+using com.playspal.core.storage;
+using com.playspal.core.utils.sound;
 
 namespace com.playspal.core
 {
     public class Core
     {
         public static float DeltaSecondsFixed = 1f / 30f;
+
+        public static Storage Storage;
+
+        static Core()
+        {
+            Storage = Storage.Load();
+            //Storage.Save(Storage);
+            //Storage = Storage.Load();
+        }
 
         public static void Update()
         {
--- system/storage/StorageLevels.cs
+++ system/storage/StorageLevels.cs
@@ -23,6 +23,20 @@ namespace com.playspal.core.storage
             Items[item.Index] = item;
         }
 
+        public void SetItemIfBetter(StorageLevel item)
+        {
+            if(Items[item.Index] == null)
+            {
+                Items[item.Index] = item;
+                return;
+            }
+
+            Items[item.Index].Completed = item.Completed;
+            Items[item.Index].ScoreEarned = Mathf.Max(Items[item.Index].ScoreEarned, item.ScoreEarned);
+            Items[item.Index].StarsEarned = Mathf.Max(Items[item.Index].StarsEarned, item.StarsEarned);
+            Items[item.Index].TimeElapsed = Mathf.Min(Items[item.Index].TimeElapsed, item.TimeElapsed);
+        }
+
         public StorageLevel GetItem(int index)
         {
             if (index < 0 || index >= Items.Length || Items[index] == null)
