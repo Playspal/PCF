e663d121b3aec9329917f69daecd22dd8589bae2
Playspal <playspal@gmail.com>
2016-09-04 21:31:33 +0300

    Added string splitting to extensions

--- system/extensions/Extensions.cs
+++ system/extensions/Extensions.cs
@@ -5,6 +5,21 @@ using System.Collections;
 
 public static class Extensions
 {
+    public static string SplitStringBySpaces(this string value, int interval = 3)
+    {
+        string output = "";
+
+        int i;
+
+        for(i = 0; i < value.Length; i+= interval)
+        {
+            output += value.Substring(i, interval);
+            output += " ";
+        }
+
+        return output;
+    }
+
     public static string IntToLeadingZerosString(this int value, int length = 2)
     {
         string valueAsString = value.ToString();
