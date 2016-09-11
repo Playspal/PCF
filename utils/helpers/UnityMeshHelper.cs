b2c8d1be0cc049b37549c7d768371950550163da
Playspal <playspal@gmail.com>
2016-09-12 01:13:47 +0300

    Added two circle shapes to mesh generator

--- utils/helpers/UnityMeshHelper.cs
+++ utils/helpers/UnityMeshHelper.cs
@@ -25,7 +25,7 @@ namespace com.playspal.core.utils.helpers
                 new Vector2(1, 1)
             };
 
-            int[] triangles = new int[] { 2, 1, 0, 1, 3, 2 };
+            int[] triangles = new int[] { 2, 1, 0, 2, 3, 1 };
 
             Mesh mesh = new Mesh();
 
@@ -35,6 +35,111 @@ namespace com.playspal.core.utils.helpers
 
             mesh.RecalculateBounds();
             mesh.RecalculateNormals();
+
+            return mesh;
+        }
+
+        public static Mesh CreateCircle(float radius = 1.0f, int items = 18)
+        {
+            Vector3[] vertices = new Vector3[360 / items + 2];
+            int verticesLength = 0;
+
+            int[] triangles = new int[vertices.Length * 3 + 3];
+            int trianglesLength = 0;
+
+            float angle;
+            int i;
+
+            vertices[verticesLength] = new Vector3(0, 0, 0);
+            verticesLength++;
+
+            for (i = 0; i < 360; i += items)
+            {
+                angle = (float)i / 180 * Mathf.PI;
+                vertices[verticesLength] = new Vector3
+                (
+                    Mathf.Cos(angle) * radius,
+                    Mathf.Sin(angle) * radius,
+                    0
+                );
+
+                verticesLength++;
+            }
+
+            for (i = 0; i < verticesLength - 1; i++)
+            {
+                triangles[trianglesLength + 2] = 0;
+                triangles[trianglesLength + 1] = i;
+                triangles[trianglesLength + 0] = i + 1;
+
+                trianglesLength += 3;
+            }
+
+            triangles[trianglesLength + 2] = 0;
+            triangles[trianglesLength + 1] = verticesLength - 1;
+            triangles[trianglesLength + 0] = 1;
+
+            Mesh mesh = new Mesh();
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
+
+            return mesh;
+        }
+
+        public static Mesh CreateCircleAdvanced(float radiusOuter = 1.0f, float radiusInner = 0.5f, int items = 18)
+        {
+            Vector3[] vertices = new Vector3[360 / items * 2];
+            int verticesLength = 0;
+
+            int[] triangles = new int[vertices.Length * 3 + 3];
+            int trianglesLength = 0;
+
+            int i;
+            float angle;
+
+            for (i = 0; i < 360; i += items)
+            {
+                angle = (float)i / 180 * Mathf.PI;
+
+                vertices[verticesLength] = new Vector3
+                (
+                    Mathf.Cos(angle) * radiusOuter,
+                    Mathf.Sin(angle) * radiusOuter,
+                    0
+                );
+
+                vertices[verticesLength + 1] = new Vector3
+                (
+                    Mathf.Cos(angle) * radiusInner,
+                    Mathf.Sin(angle) * radiusInner,
+                    0
+                );
+
+                verticesLength += 2;
+            }
+
+            for (i = 0; i < verticesLength - 2; i += 2)
+            {
+                triangles[trianglesLength + 0] = i;
+                triangles[trianglesLength + 1] = i + 1;
+                triangles[trianglesLength + 2] = i + 2;
+                triangles[trianglesLength + 5] = i + 1;
+                triangles[trianglesLength + 4] = i + 2;
+                triangles[trianglesLength + 3] = i + 3;
+
+                trianglesLength += 6;
+            }
+
+            triangles[trianglesLength + 0] = verticesLength - 2;
+            triangles[trianglesLength + 1] = verticesLength - 1;
+            triangles[trianglesLength + 2] = 0;
+            triangles[trianglesLength + 5] = verticesLength - 1;
+            triangles[trianglesLength + 4] = 0;
+            triangles[trianglesLength + 3] = 1;
+
+            Mesh mesh = new Mesh();
+            mesh.vertices = vertices;
+            mesh.triangles = triangles;
 
             return mesh;
         }
