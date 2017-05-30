38d4fc261fbc2c3ba63c26208e57578b594b39ff
Playspal <playspal@gmail.com>
2017-05-30 23:29:48 +0300

    Position tween implemented

--- ui/tweens/TweenUiPosition.cs
+++ ui/tweens/TweenUiPosition.cs
@@ -5,5 +5,23 @@ namespace com.playspal.core.ui.tweens
 {
     public class TweenUiPosition : Tween
     {
+        private RectTransform _rectTransform;
+
+        public TweenUiPosition(GameObject screen, float current, float target, float time)
+        {
+            _rectTransform = screen.GetComponent<RectTransform>();
+
+            OnTick = OnTickHandler;
+
+            Run(current, target, time);
+        }
+
+        private void OnTickHandler(float value)
+        {
+            if (_rectTransform != null)
+            {
+                _rectTransform.anchoredPosition = new Vector2(value, _rectTransform.anchoredPosition.y);
+            }
+        }
     }
 }
