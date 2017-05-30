7b0a69aff8a4144fb0fe30d4cb53e2e91463bc39
Playspal <playspal@gmail.com>
2017-05-30 22:31:53 +0300

    UnityFileSystemHelper added

---
+++ utils/helpers/UnityFileSystemHelper.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+namespace com.playspal.core.utils.helpers
+{
+    public class UnityFileSystemHelper
+    {
+        public static string RootDirectory
+        {
+            get
+            {
+                string output = "";
+
+                if (Application.platform == RuntimePlatform.Android || Application.platform == RuntimePlatform.IPhonePlayer)
+                {
+                    output = Application.persistentDataPath + "/";
+                }
+                else
+                {
+                    output = Application.dataPath + "/";
+                }
+
+                return output;
+            }
+        }
+    }
+}
