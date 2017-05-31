7e3afd48d116a0dd3b279dd18ab5ae6d550d6a64
Playspal <playspal@gmail.com>
2017-05-31 21:57:47 +0300

    CoreSettings added and implemented to UiRoot

---
+++ CoreSettings.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace com.playspal.core
+{
+    public class CoreSettings : MonoBehaviour
+    {
+        public RectTransform Canvas;
+        public List<RectTransform> CanvasLayers = new List<RectTransform>();
+
+        public static CoreSettings Instance
+        {
+            get
+            {
+                return FindObjectOfType(typeof(CoreSettings)) as CoreSettings;
+            }
+        }
+    }
+}
--- ui/UiRoot.cs
+++ ui/UiRoot.cs
@@ -8,27 +8,11 @@ namespace com.playspal.core.ui
 	{
         public static float DefinedScreenWidth = 1280f;
 
-        public static RectTransform layer1
-        {
-            get
-            {
-                return GameObject.Find("Canvas/layer1").GetComponent<RectTransform>();
-            }
-        }
-
-        public static RectTransform layer2
-        {
-            get
-            {
-                return GameObject.Find("Canvas/layer2").GetComponent<RectTransform>();
-            }
-        }
-
         public static RectTransform canvas
         {
             get
             {
-                return GameObject.Find("Canvas").GetComponent<RectTransform>();
+                return CoreSettings.Instance.Canvas;
             }
         }
 
@@ -36,13 +20,13 @@ namespace com.playspal.core.ui
         {
             get
             {
-                return GameObject.Find("Canvas").GetComponent<Canvas>().scaleFactor;
+                return CoreSettings.Instance.Canvas.GetComponent<Canvas>().scaleFactor;
             }
         }
 
         public static RectTransform GetLayer(int n)
         {
-            return GameObject.Find("Canvas/layer" + n).GetComponent<RectTransform>();
+            return CoreSettings.Instance.CanvasLayers[n - 1];
         }
 	}
 }
