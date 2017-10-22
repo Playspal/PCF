dff3bf37ec270a8c22a058273e3d5586af24704d
Playspal <playspal@gmail.com>
2017-10-22 21:03:25 +0300

    Localization draft added

---
+++ localization/Localization.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace com.playspal.core.localization
+{
+    public class Localization
+    {
+        public static Action OnLanguageChanged;
+
+        private static Dictionary<string, string> _data = new Dictionary<string, string>();
+
+        public static void SetLanguage()
+        {
+            OnLanguageChanged.InvokeIfNotNull();
+        }
+
+        public static string Get(string key)
+        {
+            if (_data.ContainsKey(key))
+            {
+                return _data[key];
+            }
+
+            return "";
+        }
+    }
+}
---
+++ localization/LocalizationTextField.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace com.playspal.core.localization
+{
+    public class LocalizationComponentText : MonoBehaviour
+    {
+        public string Key;
+
+        public void Start()
+        {
+            Localization.OnLanguageChanged += OnLanguageChanged;
+        }
+
+        private void OnLanguageChanged()
+        {
+            GetComponent<Text>().text = Localization.Get(Key);
+        }
+    }
+}
