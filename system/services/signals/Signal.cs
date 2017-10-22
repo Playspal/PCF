6e578ea47301c57bc82c3aabd73e8605d3d21f81
Playspal <playspal@gmail.com>
2017-10-22 14:33:26 +0300

    Signals service added

---
+++ system/services/signals/Signal.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace com.playspal.core.system.services.signals
+{
+    public class Signal
+    {
+        public string Name;
+        public Action Handler;
+    }
+}
---
+++ system/services/signals/Signals.cs
@@ -0,0 +1,83 @@
+﻿using System;
+
+namespace com.playspal.core.system.services.signals
+{
+    public static class Signals
+    {
+        private static Signal[] _signals = new Signal[1024];
+        private static int _signalsCount = 0;
+
+        /// <summary>
+        /// Dispatch signal with provided name
+        /// </summary>
+        /// <param name="name">Signal name</param>
+        public static void Dispatch(string name)
+        {
+            Signal signal = GetOrCreateSignal(name);
+            signal.Handler.InvokeIfNotNull();
+        }
+
+        /// <summary>
+        /// Subscribe on provided signal
+        /// </summary>
+        /// <param name="name">Signal name</param>
+        /// <param name="handler">Signal handler</param>
+        public static void AddListener(string name, Action handler)
+        {
+            Signal signal = GetOrCreateSignal(name);
+            signal.Handler += handler;
+        }
+
+        /// <summary>
+        /// Unsubscribe from provided signal
+        /// </summary>
+        /// <param name="name">Signal name</param>
+        /// <param name="handler">Signal handler</param>
+        public static void RemoveListener(string name, Action handler)
+        {
+            Signal signal = GetOrCreateSignal(name);
+            signal.Handler -= handler;
+        }
+
+        // Searches for signal, creates new one if signal not found
+        private static Signal GetOrCreateSignal(string name)
+        {
+            Signal signal = GetSignal(name);
+
+            if(signal == null)
+            {
+                signal = CreateSignal(name);
+            }
+
+            return signal;
+        }
+
+        // Searches for signal
+        private static Signal GetSignal(string name)
+        {
+            for(int i = 0; i < _signalsCount; i++)
+            {
+                if(_signals[i].Name == name)
+                {
+                    return _signals[i];
+                }
+            }
+
+            return null;
+        }
+
+        // Creates new signal
+        private static Signal CreateSignal(string name)
+        {
+            Signal signal = new Signal
+            {
+                Name = name
+            };
+
+            _signals[_signalsCount] = signal;
+            _signalsCount++;
+
+            return signal;
+        }
+    }
+}
