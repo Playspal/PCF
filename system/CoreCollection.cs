4f39826b0e67c7822d102030965d074c80009e08
Playspal <playspal@gmail.com>
2017-10-23 22:56:58 +0300

    CoreCollection updated

--- system/CoreCollection.cs
+++ system/CoreCollection.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using System;
 
 public class CoreCollection<T>
 {
@@ -57,7 +55,7 @@ public class CoreCollection<T>
     }
 
     /// <summary>
-    /// Removes the specified index
+    /// Removes item with the specified index
     /// </summary>
     /// <param name="index">The index</param>
     public void Remove(int index)
@@ -78,5 +76,35 @@ public class CoreCollection<T>
     {
         _items[_itemsCount] = item;
         _itemsCount++;
+    }
+
+    /// <summary>
+    /// Gets the index of specified item
+    /// </summary>
+    /// <param name="item">Item</param>
+    /// <returns>Index of specified item</returns>
+    public int GetIndex(T item)
+    {
+        for (int i = 0; i < _itemsCount; i++)
+        {
+            if (_items[i].Equals(item))
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    /// <summary>
+    /// Dispathes the specified function with each of items
+    /// </summary>
+    /// <param name="action">Function that receives item as parameter</param>
+    public void Dispath(Action<T> action)
+    {
+        for (int i = 0; i < _itemsCount; i++)
+        {
+            action.InvokeIfNotNull(_items[i]);
+        }
     }
 }
