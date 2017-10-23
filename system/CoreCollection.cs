630030c4bd30dfbd990a3b1460acbeaf92baf0e2
Playspal <playspal@gmail.com>
2017-10-23 23:00:33 +0300

    Namespace added to CoreCollection

--- system/CoreCollection.cs
+++ system/CoreCollection.cs
@@ -1,110 +1,113 @@
 ﻿using System;
 
-public class CoreCollection<T>
+namespace com.playspal.core.system
 {
-    private T[] _items;
-    private int _itemsCount = 0;
-
-    public T this[int index]
+    public class CoreCollection<T>
     {
-        get
-        {
-            return _items[index];
-        }
+        private T[] _items;
+        private int _itemsCount = 0;
 
-        set
+        public T this[int index]
         {
-            _items[index] = value;
-        }
-    }
-
-    /// <summary>
-    /// Gets the length of collection
-    /// </summary>
-    public int Length
-    {
-        get
-        {
-            return _itemsCount;
-        }
-    }
-
-    /// <summary>
-    /// Initializes a new instance of the <see cref="CoreCollection{T}"/> class
-    /// </summary>
-    /// <param name="length">Collection max size</param>
-    public CoreCollection(int length)
-    {
-        _items = new T[length];
-    }
-
-    /// <summary>
-    /// Removes the specified item
-    /// </summary>
-    /// <param name="item">Item</param>
-    public void Remove(T item)
-    {
-        for(int i = 0; i < _itemsCount; i++)
-        {
-            if(_items[i].Equals(item))
+            get
             {
-                Remove(i);
-                return;
+                return _items[index];
             }
-        }
-    }
 
-    /// <summary>
-    /// Removes item with the specified index
-    /// </summary>
-    /// <param name="index">The index</param>
-    public void Remove(int index)
-    {
-        _itemsCount--;
-
-        for (int i = index; i < _itemsCount; i++)
-        {
-            _items[i] = _items[i + 1];
-        }
-    }
-
-    /// <summary>
-    /// Pushes item to collection
-    /// </summary>
-    /// <param name="item">Item</param>
-    public void Add(T item)
-    {
-        _items[_itemsCount] = item;
-        _itemsCount++;
-    }
-
-    /// <summary>
-    /// Gets the index of specified item
-    /// </summary>
-    /// <param name="item">Item</param>
-    /// <returns>Index of specified item</returns>
-    public int GetIndex(T item)
-    {
-        for (int i = 0; i < _itemsCount; i++)
-        {
-            if (_items[i].Equals(item))
+            set
             {
-                return i;
+                _items[index] = value;
             }
         }
 
-        return -1;
-    }
-
-    /// <summary>
-    /// Dispathes the specified function with each of items
-    /// </summary>
-    /// <param name="action">Function that receives item as parameter</param>
-    public void Dispath(Action<T> action)
-    {
-        for (int i = 0; i < _itemsCount; i++)
+        /// <summary>
+        /// Gets the length of collection
+        /// </summary>
+        public int Length
         {
-            action.InvokeIfNotNull(_items[i]);
+            get
+            {
+                return _itemsCount;
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CoreCollection{T}"/> class
+        /// </summary>
+        /// <param name="length">Collection max size</param>
+        public CoreCollection(int length)
+        {
+            _items = new T[length];
+        }
+
+        /// <summary>
+        /// Removes the specified item
+        /// </summary>
+        /// <param name="item">Item</param>
+        public void Remove(T item)
+        {
+            for (int i = 0; i < _itemsCount; i++)
+            {
+                if (_items[i].Equals(item))
+                {
+                    Remove(i);
+                    return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Removes item with the specified index
+        /// </summary>
+        /// <param name="index">The index</param>
+        public void Remove(int index)
+        {
+            _itemsCount--;
+
+            for (int i = index; i < _itemsCount; i++)
+            {
+                _items[i] = _items[i + 1];
+            }
+        }
+
+        /// <summary>
+        /// Pushes item to collection
+        /// </summary>
+        /// <param name="item">Item</param>
+        public void Add(T item)
+        {
+            _items[_itemsCount] = item;
+            _itemsCount++;
+        }
+
+        /// <summary>
+        /// Gets the index of specified item
+        /// </summary>
+        /// <param name="item">Item</param>
+        /// <returns>Index of specified item</returns>
+        public int GetIndex(T item)
+        {
+            for (int i = 0; i < _itemsCount; i++)
+            {
+                if (_items[i].Equals(item))
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+
+        /// <summary>
+        /// Dispathes the specified function with each of items
+        /// </summary>
+        /// <param name="action">Function that receives item as parameter</param>
+        public void Dispath(Action<T> action)
+        {
+            for (int i = 0; i < _itemsCount; i++)
+            {
+                action.InvokeIfNotNull(_items[i]);
+            }
         }
     }
 }
